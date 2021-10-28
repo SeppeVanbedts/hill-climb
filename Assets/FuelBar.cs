4d4806eab25910722d8df1bb8f05a4827b4be230
SeppeVanbedts <92398172+SeppeVanbedts@users.noreply.github.com>
2021-10-28 15:20:00 +0200

    change 6.0

--- Assets/FuelBar.cs
+++ Assets/FuelBar.cs
@@ -1,26 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
 
 public class FuelBar : MonoBehaviour
 {
-	public Slider slider;
-	public Gradient gradient;
-	public Image fill;
+    public float fuel = 1;
+    public float fuelconsumption = 0.1F;
 
-	public void SetMaxHealth(int health)
-	{
-		slider.maxValue = health;
-		slider.value = health;
+    private void FixedUpdate()
+    {
+        if(fuel > 0)
+        {
 
-		fill.color = gradient.Evaluate(1f);
-	}
-
-	public void SetHealth(int health)
-	{
-		slider.value = health;
-
-		fill.color = gradient.Evaluate(slider.normalizedValue);
-	}
+        }
+    }
 }
